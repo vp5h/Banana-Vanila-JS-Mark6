131ea86d0eeb7752e63a5aa03ae1fb5a8d93d69f
Pravesh Vyas <praveshvyas.18@gmail.com>
2021-08-04 01:40:32 +0530

    Completed

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
 
 var btnTranslate = document.querySelector("#btn-translate")
-var txtInput = document.querySelector("#txt-input")
+var txtInput = document.querySelector("#text-input")
 var outputDiv = document.querySelector("#output")
+var inputText= txtInput.value;
+// console.log(txtInput)
 
-console.log(txtInput)
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+function getTranslationURL(text){
+        return serverURL + "?" + "text=" + text ;
+
+}
+
+
+function errorHandler(error){
+   
+    console.log("Error Occured", error)
+   
+}
 
 function clickHandler() {
-   outputDiv.innerText = "ajsjsjsjsjs " + txtInput.value;
+//    outputDiv.innerText = "ajsjsjsjsjs " + txtInput.value;
+var inputText= txtInput.value;
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        // if(json.status= 429){
+        //     var err= json.error.message;
+        //     outputDiv.innerText = err;
+        //     console.log(err)
+       
+       
+           
+        // } 
+        // else{
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        // }
+     })
+    
+       
+    .catch(errorHandler)
     
 }
 
